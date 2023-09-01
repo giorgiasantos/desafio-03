cad1b7585f8b65d84f6632b8f1a6cad847f89bfc
giorgiasantos <giorgia.santos@zup.com.br>
2023-09-01 13:27:03 -0300

    Subindo o código da aplicação

--- src/main/java/com/example/catalisa/desafio3/mapper/ProdutosMapper.java
+++ src/main/java/com/example/catalisa/desafio3/mapper/ProdutosMapper.java
@@ -9,9 +9,9 @@ import org.mapstruct.factory.Mappers;
 @Mapper
 public interface ProdutosMapper {
     ProdutosMapper INSTANCE = Mappers.getMapper(ProdutosMapper.class);
-    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "id", ignore = false)
     ProdutosDTO ProdutosModelToProdutoDTO(ProdutosModel produtosModel);
-    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "id", ignore = false)
     ProdutosModel produtosDTOToProdutosModel(ProdutosDTO produtosDTO);
 
 }
